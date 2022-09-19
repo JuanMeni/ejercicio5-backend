f47694643be0584e053fc510cb837caa533ec4eb
Juan Menichetti <jpmenichetti13@gmail.com>
2022-09-19 15:14:25 -0300

    agregamos middlewares para diferentes config

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 // const express = require('express');
 
@@ -14,11 +16,19 @@ app.listen(app.get('port'), ()=>{
     console.log('Mi backend esta en el puerto ' + app.get('port'));
 })
 // middlewares
+app.use(morgan('dev')); //da informacion en la terminal
+app.use(cors()); //permite recibir peticiones remotas de otras pc
+// los dos middlewares sgtes. sirven para procesar un obj json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//cargar un archivo estatico
+app.use(express.static('./public'));
+
 
 // rutas
-app.get('/', (req,res)=>{
-    res.send('Primera peticion get')
-})
+// app.get('/', (req,res)=>{
+//     res.send('Primera peticion get')
+// })
 
 app.get('/prueba', (req,res)=>{
     res.send('Otra peticion get')

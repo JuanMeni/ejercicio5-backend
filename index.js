a7635dd69fdb8e0fc2a4dc57c875d8fd8cf0defa
Juan Menichetti <jpmenichetti13@gmail.com>
2022-09-19 15:00:54 -0300

    agregamos rutas

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.listen(app.get('port'), ()=>{
 
     console.log('Mi backend esta en el puerto ' + app.get('port'));
 })
+// middlewares
+
+// rutas
+app.get('/', (req,res)=>{
+    res.send('Primera peticion get')
+})
+
+app.get('/prueba', (req,res)=>{
+    res.send('Otra peticion get')
+})
 
 
 
